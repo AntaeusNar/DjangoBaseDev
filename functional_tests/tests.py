9399f98d9b92b4389807aeba85e54e8be8699daf
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-05-09 09:23:44 -0700

    expanded user story for functional test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,6 @@ each section should follow an human readable 'user story'
 """
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-
 from selenium import webdriver
 import os
 
@@ -27,5 +26,10 @@ class IndexTest(StaticLiveServerTestCase):
         response = self.client.get(self.live_server_url)
         self.assertEqual(response.status_code, 200)
 
+        # Edith, being smarter then the average bear, notices that the site is using https.
+
+        # Pleased, Edith sees that the site has a list of sub sites, with a description of each.
+
     def test_finish_functional_tests(self):
         self.fail('Finish the Functional Test!')
+
