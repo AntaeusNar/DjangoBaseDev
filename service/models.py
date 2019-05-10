e398b6ddef55432f3828e2e811fc1170578cd0d9
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-05-10 08:36:55 -0700

    added Event model

--- service/models.py
+++ service/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    # this is the core of the service system, each event is the what, when, where and how of the system
+    pass
