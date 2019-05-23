274d1814efd8d0cabee24ceb1802d064b8947ebc
AntaeusNar <28239049+AntaeusNar@users.noreply.github.com>
2019-05-23 13:35:22 -0700

    created basic view

--- service/views.py
+++ service/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 
 def dashboard(request):
     return render(request, 'service/dashboard.html')
+
+
+def registration(request):
+    return render(request, 'registration/register.html')
