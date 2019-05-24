5fc80cba2d203b17d4c59abb70453e143271c7c6
AnteusNar <jedidiah.h.wallace@gmail.com>
2019-05-24 13:47:33 -0700

    expanded dashboard view to pass Event lists

--- service/views.py
+++ service/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from service.forms import UserCreationForm
+from service.models import Event
 
 # Create your views here.
 
 
 def dashboard(request):
-    return render(request, 'service/dashboard.html')
+    events = Event.objects.all
+    context = {'events': events}
+    return render(request, 'service/dashboard.html', context)
 
 
 def registration(request):
