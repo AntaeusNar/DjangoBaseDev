f7aaa4c8154f87accc41badaf1955762a42ccbd1
Jedidiah Wallace <jedidiah.h.wallace@gmail.com>
2019-06-03 14:11:48 -0700

    expanded parts testing

--- service/tests/tests_base_models.py
+++ service/tests/tests_base_models.py
@@ -182,6 +182,27 @@ class PartModelTest(TestCase):
         self.assertEqual(first_saved_part.name, 'Flux Capacitor')
         self.assertEqual(first_saved_part.name, str(first_saved_part))
 
+    def test_add_parts_to_parts(self):
+        first_part = Part()
+        first_part.name = "Command Access Motor"
+
+        first_part.save()
+
+        second_part = Part()
+        second_part.name = "RB12/24"
+
+        second_part.save()
+
+        parent_part = Part()
+        parent_part.name = "Von Duprin Panic Bar"
+        parent_part.child = first_part
+        parent_part.child = second_part
+
+        parent_part.save()
+
+        saved_parts = Part.objects.all()
+        self.assertEqual(saved_parts.count(), 3)
+
 
 class ContainerModelTest(TestCase):
 
